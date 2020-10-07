20cce10fa759f7add261ab04bc2cdf944b2f3aee
Artemii Yanushevskyi <artemii@nevercode.io>
2020-10-07 16:06:18 +0300

    Initial Commit

---
+++ MyApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyApp
+//
+//  Created by Artemii Yanushevskyi on 10/7/20.
+//  Copyright © 2020 Artemii Yanushevskyi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
